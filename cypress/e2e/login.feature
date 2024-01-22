5eaca9f77b5f9b144497c330a65e3f222290926e
luanamuller <luana.muller@acct.global>
2024-01-22 17:00:31 -0300

    ajustes etapa login incorreto projeto JJ Veste

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -4,10 +4,10 @@ Feature: Login tests
     Background:
         Given I'm on the login page
 
-    Scenario: Login with valid e-mail and password
-        When I do login using correct email and password
-        Then I must be logged on the site
+   # Scenario: Login with valid e-mail and password
+    #    When I do login using correct email and password
+    #  Then I must be logged on the site
 
-    # Scenario: Login with incorrect password
-     #   When I do login using incorrect password
-     #  Then Must be informed that the data access are wrong
+    Scenario: Login with incorrect password
+        When I do login using incorrect password
+        Then Must be informed that the data access are wrong
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -3,7 +3,7 @@ import {Before, Given, When, And, Then} from "cypress-cucumber-preprocessor/step
 Given("I'm on the login page", () => {
     cy.visit("https://www.johnjohndenim.com.br/login");
 })
-
+/*
 When("I do login using correct email and password", () => {
    cy.get('.vtex-login-2-x-emailPasswordOptionBtn').click();
    cy.get('.vtex-login-2-x-inputContainerEmail > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input').click().type('luana.muller@acct.global');
@@ -13,4 +13,14 @@ When("I do login using correct email and password", () => {
 Then("I must be logged on the site", () => {
     cy.get('.vtex-login-2-x-sendButton > .vtex-button').click();
 })
+*/
+When("I do login using incorrect password", () => {
+    cy.get('.vtex-login-2-x-emailPasswordOptionBtn').click();
+    cy.get('.vtex-login-2-x-inputContainerEmail > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input').click().type('luanamuller@acct.global');
+    cy.get('.relative > .vtex-input > .vtex-input-prefix__group > .vtex-styleguide-9-x-input').click().type('Vbc71@44');
+ })
 
+ Then("Must be informed that the data access are wrong", () => {
+    cy.get('.vtex-login-2-x-sendButton > .vtex-button').click()
+    //cy.contains(vtex-login-2-x-formError).should('be.visible')
+})
